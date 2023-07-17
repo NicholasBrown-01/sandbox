2b52d5ec6e1f49a53b413380873e2ffc858a6dbe
NicholasBrown-01 <nicholasbrown.us@gmail.com>
2023-07-16 20:59:20 -0500

    File Structure Updated with LinkedList Test and Node files

---
+++ gradleProjects/applications/app/src/main/java/applications/LinkedList.java
@@ -0,0 +1,11 @@
+package applications;
+
+public class LinkedList {
+
+    public void insert(String) {
+
+    LinkedList<String> namesLinkedList = new LinkedList();
+
+
+    }
+}
---
+++ gradleProjects/applications/app/src/test/java/applications/LinkedListTEST.java
@@ -0,0 +1,19 @@
+package applications;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LinkedListTEST {
+
+   @Test
+    void createLinkedListTEST() {
+        LinkedList sut = new LinkedList();
+        sut.insert("Nick");
+        sut.insert("Sara");
+        System.out.println(value);
+
+    }
+
+
+}
